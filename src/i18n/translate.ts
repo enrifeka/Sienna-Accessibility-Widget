399706c8615a3c2eca018d79558f1995618211d4
enri.feka <enri.feka@horizon.al>
2025-08-22 11:19:42 +0200

    change default translation to albanian if not found

--- src/i18n/translate.ts
+++ src/i18n/translate.ts
@@ -2,6 +2,6 @@ import { LANGUAGE_DICTIONARY } from "./Languages";
 import { userSettings } from "../globals/userSettings";
 
 export function t(label: string): string {
-    const dictionary = LANGUAGE_DICTIONARY[userSettings.lang] ?? LANGUAGE_DICTIONARY["en"];
+    const dictionary = LANGUAGE_DICTIONARY[userSettings.lang] ?? LANGUAGE_DICTIONARY["sq"];
     return dictionary[label] ?? label;
 }
