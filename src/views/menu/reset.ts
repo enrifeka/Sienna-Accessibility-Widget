4918b029e593dd4e47122c281f1f71e17f74cea8
enri.feka <enri.feka@horizon.al>
2025-08-22 18:03:45 +0200

    changed the fontSize reset

--- src/views/menu/reset.ts
+++ src/views/menu/reset.ts
@@ -2,7 +2,12 @@ import { saveUserSettings, userSettings } from "@/globals/userSettings";
 import runAccessibility from "./runAccessibility";
 
 export default function reset() {
-    document?.querySelectorAll(".asw-selected")?.forEach(el => el?.classList?.remove("asw-selected"))
+    document?.querySelectorAll(".asw-selected")?.forEach(el => el?.classList?.remove("asw-selected"));
+
+    const amountEl = document.querySelector('.asw-amount');
+    if (amountEl) {
+        amountEl.textContent = "100%"; // or your default value
+    }
 
     userSettings.states = {};
     runAccessibility();
